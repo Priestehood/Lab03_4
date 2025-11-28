0f0ec7f9bb703b4871a2a5f594466b2454585520
JerryChen543 <245104591+JerryChen543@users.noreply.github.com>
2025-11-28 18:01:41 +0800

    feat: “高程分析”-“高程点滤波”
    - KNearestNeighbor辅助类，用于搜索点要素的K近邻
    - ElevationAnalysis服务类，用于对选定高程点图层进行高程分析，包括“高程点滤波”和“高程计算”（待实现）

---
+++ MyForms/SpatialQuery/Helpers/KNearestNeighbor.cs
@@ -0,0 +1,104 @@
+﻿using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.Geodatabase;
+using System;
+using System.Collections.Generic;
+
+namespace Lab04_4.MyForms.SpatialQuery.Helpers
+{
+    class KNearestNeighbor
+    {
+        public class FeatureDistancePair
+        {
+            public IFeature feature;
+            public double distance;
+
+            public FeatureDistancePair(IFeature feature, double distance)
+            {
+                this.feature = feature;
+                this.distance = distance;
+            }
+        }
+
+        public static List<FeatureDistancePair> FindKNearest(
+            IPoint point, int k, IFeatureClass featureClass,
+            int excludeOID = -1)
+        {
+            if (k <= 0) throw new Exception("k应为正整数");
+
+            // 初始化 要素-距离 列表
+            List<FeatureDistancePair> candidates = new List<FeatureDistancePair>();
+            // 初始的搜索范围半径
+            double range = 2.5;
+            int loop = 0;
+            // 在搜索结果不足k个时
+            while (candidates.Count < k)
+            {
+                if (loop++ > 20)
+                    return null;
+
+                // 扩大搜索范围
+                range *= 2;
+                // 如果需要排除目标点，根据传入的OID构建where表达式
+                string whereClause = "";
+                if (excludeOID != -1)
+                {
+                    whereClause =
+                        string.Format("{0} <> {1}",
+                        featureClass.OIDFieldName, excludeOID.ToString());
+                }
+                // 建立缓冲区，进行搜索
+                IFeatureCursor cursor =
+                    SearchFeatureInRange(point, range, featureClass, whereClause);
+                // 计算距离，更新列表
+                UpdateCandidates(point, cursor, candidates);
+            }
+
+            // 抛弃多余的结果
+            candidates.RemoveRange(k, candidates.Count - k);
+
+            return candidates;
+        }
+
+        static IFeatureCursor SearchFeatureInRange(
+            IPoint point, double distance,
+            IFeatureClass fc, string whereClause = "")
+        {
+            // 创建缓冲区
+            ITopologicalOperator topoOperator = point as ITopologicalOperator;
+            IGeometry searchArea = topoOperator.Buffer(distance);
+
+            // 设置空间过滤器
+            ISpatialFilter filter = new SpatialFilter
+            {
+                Geometry = searchArea,
+                SpatialRel = esriSpatialRelEnum.esriSpatialRelIntersects,
+                WhereClause = whereClause
+            };
+
+            // 对要素类执行查询
+            IFeatureCursor cursor = fc.Search(filter, false);
+            return cursor;
+        }
+
+        static void UpdateCandidates(IPoint source, IFeatureCursor cursor,
+            List<FeatureDistancePair> candidates)
+        {
+            IFeature feature;
+            while ((feature = cursor.NextFeature()) != null)
+            {
+                // 如果点要素已经存在则跳过
+                //if (candidates.ConvertAll(c=>c.feature).Contains(feature))
+                if (candidates.ConvertAll(c => c.feature.OID).Contains(feature.OID))
+                    continue;
+
+                // 计算每个点要素到目标点的距离
+                IPoint point = feature.Shape as IPoint;
+
+
+                double distance = (source as IProximityOperator).ReturnDistance(point);
+                candidates.Add(new FeatureDistancePair(feature, distance));
+                candidates.Sort((a, b) => a.distance.CompareTo(b.distance));
+            }
+        }
+    }
+}
---
+++ MyForms/SpatialQuery/Services/ElevationAnalysis.cs
@@ -0,0 +1,115 @@
+﻿using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.Geodatabase;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Lab04_4.MyForms.SpatialQuery.Helpers;
+
+namespace Lab04_4.MyForms.SpatialQuery.Services
+{
+    class ElevationAnalysis
+    {
+        private IFeatureLayer elevPointLayer;
+        private string ZFieldName = "Z";
+
+        public ElevationAnalysis(IFeatureLayer layer)
+        {
+            this.elevPointLayer = layer;
+        }
+
+        public int DetectAbnormalElevations(int n)
+        {
+            IFeatureCursor cursor = elevPointLayer.FeatureClass.Update(null, true);
+
+            int deleteCount = 0;
+            IFeature elevationPoint;
+            while ((elevationPoint = cursor.NextFeature()) != null)
+            {
+                List<IFeature> NN = GetKNN(elevationPoint, n);
+                if (IsOutlier(elevationPoint, NN))
+                {
+                    elevationPoint.Delete();
+                    deleteCount++;
+                }
+            }
+
+            return deleteCount;
+        }
+
+        /// <summary>
+        /// 搜索目标高程点最近的n个高程点，组成N近邻
+        /// </summary>
+        /// <param name="target">目标高程点</param>
+        /// <param name="n">搜索最近高程点的数量n</param>
+        /// <returns></returns>
+        private List<IFeature> GetKNN(IFeature target, int n)
+        {
+            if (!(target.Shape is IPoint)) throw new Exception("指定要素非点要素");
+
+            List<KNearestNeighbor.FeatureDistancePair> results =
+                KNearestNeighbor.FindKNearest(target.Shape as IPoint, n, elevPointLayer.FeatureClass,
+                target.OID);
+            if (results is null) return null;
+
+            List<IFeature> features = new List<IFeature>();
+            return results.ConvertAll((pair) => pair.feature);
+        }
+
+        /// <summary>
+        /// 判断目标高程点是否为离群点
+        /// </summary>
+        /// <param name="target">目标高程点</param>
+        /// <param name="NN">目标高程点的N近邻</param>
+        /// <returns></returns>
+        private bool IsOutlier(IFeature target, List<IFeature> NN)
+        {
+            NN.Add(target);
+            // 获取高程值
+            List<double> elevations = GetElevations(NN);
+
+            // 计算平均值、标准差
+            double avg = elevations.Average();
+            double sumOfSquares = elevations
+                .Select(e => Math.Pow(e - avg, 2))
+                .Sum();
+            double variance = sumOfSquares / (NN.Count - 1);
+            double std = Math.Sqrt(variance);
+
+            // 计算容差范围
+            double avgPlus3Std = avg + 3 * std;
+            double avgMinus3Std = avg - 3 * std;
+            double targetElevation = elevations.Last();
+
+            return targetElevation < avgMinus3Std ||
+                targetElevation > avgPlus3Std;
+        }
+
+        /// <summary>
+        /// 将高程点的要素列表转换为高程值列表
+        /// </summary>
+        /// <param name="features">高程点的要素列表</param>
+        /// <returns>高程值列表</returns>
+        private List<double> GetElevations(List<IFeature> features)
+        {
+            int ZFieldIndex = features[0].Fields.FindField(ZFieldName);
+            List<double> elevations = new List<double>();
+            foreach (IFeature feature in features)
+            {
+                double elevation = (double)feature.Value[ZFieldIndex];
+                elevations.Add(elevation);
+            }
+            return elevations;
+        }
+
+        public double IntepolateElevation(IPoint click, int n)
+        {
+            List<KNearestNeighbor.FeatureDistancePair> results =
+                KNearestNeighbor.FindKNearest(click, n, elevPointLayer.FeatureClass);
+
+            // TODO: 反距离内插法
+            double elevation = 0;
+            return elevation;
+        }
+    }
+}
